a46bf024b377a142b28983277c66ed7a55d12c98
lttviet <lttviet@gmail.com>
2016-06-16 13:40:15 -0400

    update to ubuntu 16.04

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 # setup.sh for configuring Ubuntu home pc
 
 # Alias
-alias purge="sudo apt-get purge -y"
-alias instal="sudo apt-get install -y"
-alias update="sudo apt-get update"
-alias upgrade="sudo apt-get upgrade"
+alias purge="sudo apt purge -y"
+alias instal="sudo apt install -y"
+alias update="sudo apt update"
+alias upgrade="sudo apt upgrade"
 
 # Remove unnecessary softwares
 purge friends* empathy totem
@@ -30,11 +30,12 @@ instal -y htop iotop mpv python-pip synaptic xclip build-essential python-dev
 instal -y redshift-gtk zram-config tlp nautilus-dropbox sublime-text-installer
 instal -y neovim iojs simplescreenrecorder python3-dev python3-pip curl
 instal -y rbenv cmake ruby-dev libtool-bin autoconf libzmq3 libzmq3-dev
-#instal -y texlive texlive-xetex
 
 # neovim plugins
 sudo pip install neovim
-#sudo pip3 install neovim
+
+# Install virtual env wrapper for Python
+sudo pip install virtualenvwrapper
 
 # patched fonts for vim-airline
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
@@ -49,30 +50,24 @@ mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# compile YouCompleteMe
-~/.config/nvim/plugged/YouCompleteMe/install.sh --clang-completer
-
 sudo tlp start
 
 # fix dropbox
 echo fs.inotify.max_user_watches=100000 | sudo tee -a /etc/sysctl.conf; sudo sysctl -p
 
-# Install virtual env wrapper for Python
-sudo pip install virtualenvwrapper
-
 # git pull and install dotfiles
 cd $HOME
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
+if [ -d ~/dotfiles/ ]; then
+    mv ~/dotfiles ~/dotfiles.old
 fi
-if [ -d .nvimrc ]; then
-    mv .nvimrc .nvimrc.old
+if [ -d ./.config/nvim/init.vim ]; then
+    mv ./.config/nvim/init.vim ./.config/nvim/init.vim.old
 fi
 git clone https://github.com/lttviet/dotfiles.git
-ln -sb ~/dotfiles/.nvimrc .config/nvim/init.vim
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/redshift.conf .config
-ln -sb dotfiles/.gitconfig .
-ln -sb dotfiles/.gitignore_global .
+ln -sb ~/dotfiles/init.vim .config/nvim/init.vim
+ln -sb ~/dotfiles/.bashrc .
+ln -sb ~/dotfiles/redshift.conf .config
+ln -sb ~/dotfiles/.gitconfig .
+ln -sb ~/dotfiles/.gitignore_global .
 
 source .bashrc
