d4c0ef9f81849994bc8b3ac4385fd984133fdfe4
lttviet <lttviet@gmail.com>
2016-09-10 17:16:33 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,19 +23,17 @@ sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
 
+# Add Syncthing
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+echo "deb http://apt.syncthing.net/ syncthing release" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
 update
 
-instal -y ubuntu-restricted-extras openjdk-8-jdk clang yasm devscripts
-instal -y htop iotop mpv python-pip synaptic xclip build-essential python-dev
-instal -y redshift-gtk zram-config tlp nautilus-dropbox sublime-text-installer
-instal -y neovim iojs simplescreenrecorder python3-dev python3-pip curl
-instal -y rbenv cmake ruby-dev libtool-bin autoconf libzmq3 libzmq3-dev
-
-# neovim plugins
-sudo pip install neovim
-
-# Install virtual env wrapper for Python
-sudo pip install virtualenvwrapper
+instal ubuntu-restricted-extras openjdk-8-jdk clang yasm devscripts apt-transport-https ca-certificates
+instal htop iotop iftop nethogs mpv python-pip synaptic xclip build-essential python-dev
+instal redshift-gtk zram-config tlp sublime-text-installer virtualenvwrapper
+instal neovim simplescreenrecorder python3-dev python3-pip curl neovim docker-engine
+instal rbenv cmake ruby-dev libtool-bin autoconf libzmq3 libzmq3-dev syncthing
 
 # patched fonts for vim-airline
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
@@ -60,11 +58,20 @@ fi
 if [ -d ./.config/nvim/init.vim ]; then
     mv ./.config/nvim/init.vim ./.config/nvim/init.vim.old
 fi
+
 git clone https://github.com/lttviet/dotfiles.git
 ln -sb ~/dotfiles/init.vim .config/nvim/init.vim
 ln -sb ~/dotfiles/.bashrc .
 ln -sb ~/dotfiles/redshift.conf .config
 ln -sb ~/dotfiles/.gitconfig .
 ln -sb ~/dotfiles/.gitignore_global .
+
+# syncthing systemd
+mkdir -p ~/.config/systemd/user
+cp ~/dotfiles/syncthing* ~/.config/systemd/user/
+systemctl --user enable syncthing.service
+systemctl --user start syncthing.service
+systemctl --user enable syncthing-inotify.service
+systemctl --user start syncthing-inotify.service
 
 source .bashrc
