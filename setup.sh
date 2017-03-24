ad622f5d2e42f956f96f1d1c8574efcbba9829f5
lttviet <lttviet@gmail.com>
2017-03-24 14:36:47 -0400

    update for ubuntu 17.04

--- setup.sh
+++ setup.sh
@@ -9,16 +9,11 @@ alias upgrade="sudo apt upgrade"
 
 # Remove unnecessary softwares
 purge totem unity-scope-openclipart unity-scope-yelp unity-scope-colourlovers
-purge evolution-data-server rhythmbox* unity-scope-zotero
-
-# Install Heroku toolbelt
-# https://toolbelt.heroku.com/debian
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+purge rhythmbox* unity-scope-zotero unity-scope-tomboy unity-scope-gdrive
+purge thunderbird
 
 # Add PPA repos
-sudo add-apt-repository -y ppa:linrunner/tlp
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
 
 # Add Syncthing
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
@@ -27,12 +22,14 @@ echo "fs.inotify.max_user_watches=204800" | sudo tee -a /etc/sysctl.conf
 
 update
 
-instal ubuntu-restricted-extras openjdk-8-jdk clang yasm devscripts apt-transport-https ca-certificates
-instal htop iotop iftop nethogs mpv python-pip synaptic xclip build-essential python-dev
-instal redshift-gtk zram-config tlp sublime-text-installer virtualenvwrapper
-instal neovim python3-dev python3-pip curl neovim docker.io docker-compose
-instal rbenv cmake ruby-dev libtool-bin autoconf libzmq3-dev syncthing keepassx
-install libssl-dev libreadline-dev zlib1g-dev
+instal ubuntu-restricted-extras openjdk-8-jdk clang yasm devscripts git
+instal neovim nethogs mpv python-pip synaptic xclip build-essential
+instal redshift-gtk sublime-text-installer htop iotop
+instal python3-dev python3-pip neovim
+instal cmake ruby-dev libtool-bin autoconf libzmq3-dev syncthing
+instal libssl-dev libreadline-dev zlib1g-dev
+
+sudo snap install keepassxc
 
 # patched fonts for vim-airline
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
@@ -46,10 +43,6 @@ mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 # vim-plug
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# neovim python plugins
-pip install --upgrade neovim
-pip3 install --upgrade neovim
 
 # symlinks dotfiles
 cd $HOME
@@ -68,7 +61,7 @@ ln -sb ~/dotfiles/.gitconfig .
 ln -sb ~/dotfiles/.gitignore_global .
 ln -sb ~/dotfiles/.tmux.conf .
 
-# syncthing systemd
+# systemd
 mkdir -p ~/.config/systemd/user
 cp ~/dotfiles/syncthing* ~/.config/systemd/user/
 systemctl --user enable syncthing.service
@@ -76,9 +69,19 @@ systemctl --user start syncthing.service
 systemctl --user enable syncthing-inotify.service
 systemctl --user start syncthing-inotify.service
 
+sudo sed -i 's/#user_allow_other/user_allow_other/' /etc/fuse.conf
+cp ~/dotfiles/rclone-mount.service ~/.config/systemd/user/
+systemctl --user enable rclone-mount.service
+systemctl --user start rclone-mount.service
+
 # rbenv install plugins
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+cd ~/.rbenv && src/configure && make -C src
 
-sudo tlp start
+#pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
-source .bashrc
+sudo sed -i 's/quiet splash//' /etc/default/grub
+sudo sed -i 's/#GRUB_TERMINAL/GRUB_TERMINAL/' /etc/default/grub
+sudo update-grub
